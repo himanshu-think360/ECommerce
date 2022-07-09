22d2b8562ef2bbb63aa9244e1ed8f94fd839df4f
himanshu.sanklecha <himanshu.sanklecha@think360.ai>
2022-07-09 21:40:20 +0530

    Updates

---
+++ precache-manifest.cbf9787d098a816d86de319cc1c41c94.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "626220143bfc6920a4f3",
+    "url": "/ECommerce/static/css/main.64273ee2.chunk.css"
+  },
+  {
+    "revision": "626220143bfc6920a4f3",
+    "url": "/ECommerce/static/js/main.62622014.chunk.js"
+  },
+  {
+    "revision": "d6ad0681366502a3b3ea",
+    "url": "/ECommerce/static/js/1.d6ad0681.chunk.js"
+  },
+  {
+    "revision": "1c5f2f1e26a4d537064a",
+    "url": "/ECommerce/static/css/2.c720b634.chunk.css"
+  },
+  {
+    "revision": "1c5f2f1e26a4d537064a",
+    "url": "/ECommerce/static/js/2.1c5f2f1e.chunk.js"
+  },
+  {
+    "revision": "d4060165410bb3048c23",
+    "url": "/ECommerce/static/js/runtime~main.d4060165.js"
+  },
+  {
+    "revision": "5d52cfd4574e25aa7fd0c45085ddd85f",
+    "url": "/ECommerce/index.html"
+  }
+];
